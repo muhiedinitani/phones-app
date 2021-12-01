b34d43bd07a2aeab4ce407e7518691512a8b1996
Dean Itani <muhiedin.itani@gmail.com>
2021-11-30 18:22:20 -0600

    finish show action

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -3,4 +3,10 @@ class PhonesController < ApplicationController
     @phones = Phone.all
     render 'index.html.erb'
   end
+
+  def show
+    @phone = Phone.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
+  
 end
