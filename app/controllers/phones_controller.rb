bb8140aab023867cd70e6de427eac0f3c022e3db
Dean Itani <muhiedin.itani@gmail.com>
2021-11-30 19:19:13 -0600

    add create+new action

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -8,5 +8,18 @@ class PhonesController < ApplicationController
     @phone = Phone.find_by(id: params[:id])
     render 'show.html.erb'
   end
+
+  def create
+    @phone = Phone.new(
+      model: params[:model],
+      version: params[:version],
+      color: params[:color]
+    )
+    @phone.save
+    redirect_to "/phones/#{@phone.id}"
+  end
   
+  def new
+    render 'new.html.erb'
+  end
 end
